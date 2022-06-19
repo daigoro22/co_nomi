5d89bdb779276dc9be37bbdde863ec5ce6edb9a2
daigoro22 <abe.daigoro.sb@tut.jp>
2022-06-19 19:27:06 +0900

    ルーティング方法を resources を使った方式に変更

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -9,4 +9,19 @@ class PartiesController < ApplicationController
   def new
     @party = Party.new
   end
+
+  def create
+    @party = Party.new(party_params)
+    if @party.save
+      redirect_to parties_url, notice: "飲み会「#{@party.name}」を登録しました"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def party_params
+    params.require(:party).permit(:name, :date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'parties/index'
-  get 'parties/edit'
-  get 'parties/new'
+  resources :parties
   root to: 'parties#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
