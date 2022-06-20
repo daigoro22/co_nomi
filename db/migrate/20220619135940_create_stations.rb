c3dcb0630db3b699643f13ca33cdcc5c68de8e8e
daigoro22 <abe.daigoro.sb@tut.jp>
2022-06-20 12:19:14 +0900

    Station モデルを作成

---
+++ db/migrate/20220619135940_create_stations.rb
@@ -0,0 +1,11 @@
+class CreateStations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :stations do |t|
+      t.string :name
+      t.decimal :lat
+      t.decimal :lng
+
+      t.timestamps
+    end
+  end
+end
