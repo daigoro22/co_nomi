7867877e5f18618f50d2fc7d158c4668a53d8b9c
daigoro22 <abe.daigoro.sb@tut.jp>
2022-06-25 22:16:55 +0900

    駅名検索ボタンから候補のラジオボタンを動的に追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,19 +15,65 @@ ActiveStorage.start();
 import "bootstrap";
 import "../stylesheets/application";
 
+const fetchStationInfo = async (name) => {
+  const query = new URLSearchParams({
+    name: name,
+  });
+  const response = await fetch(
+    `http://express.heartrails.com/api/json?method=getStations&${query}`
+  );
+  return await response.json();
+};
+
+function log(e) {
+  console.log(e);
+}
+
 window.addEventListener("load", () => {
   const button = document.querySelector("#btn-search");
   const stationName = document.querySelector("#station-name");
+  const stationRadioContainer = document.querySelector("#radio-container");
 
   button.addEventListener("click", async (event) => {
     event.preventDefault();
-    const query = new URLSearchParams({
-      name: stationName.value,
-    });
-    const response = await fetch(
-      `http://express.heartrails.com/api/json?method=getStations&${query}`
-    );
-    const data = await response.json();
+    const data = await fetchStationInfo(stationName.value);
     console.log(data);
+
+    const stations = data.response.station;
+
+    const onStationNameChange = (event) => {
+      const info = stations[event.target.value];
+      console.log(info);
+    };
+
+    // ラジオボタンのコンテナ内の要素を削除
+    while (stationRadioContainer.firstChild) {
+      stationRadioContainer.removeChild(stationRadioContainer.firstChild);
+    }
+
+    // ラジオボタンを追加
+    stations.map((st, i) => {
+      const rbContainer = document.createElement("div");
+
+      const rb = document.createElement("input");
+      rb.type = "radio";
+      rb.name = "stations";
+      rb.value = i;
+
+      const lb = document.createElement("label");
+      lb.for = i;
+      const lbText = `${st.name}（${st.line}）`;
+      lb.appendChild(document.createTextNode(lbText));
+
+      rbContainer.appendChild(rb);
+      rbContainer.appendChild(lb);
+      stationRadioContainer.appendChild(rbContainer);
+    });
+
+    // ラジオボタンの onChange イベントで hidden 要素を変化させる
+    const radios = stationRadioContainer.children;
+    Array.prototype.forEach.call(radios, (e) => {
+      e.addEventListener("change", onStationNameChange);
+    });
   });
 });
