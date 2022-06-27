1783ec282708787e858ed8db6f1d98bec123e0b8
daigoro22 <abe.daigoro.sb@tut.jp>
2022-06-27 20:55:02 +0900

    fix #9 Shop のコントローラを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'shops/create'
+  get 'shops/destroy'
   resources :parties
   root to: 'parties#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
